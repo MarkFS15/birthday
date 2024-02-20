2dd91c1fac138bdac1f4005700e6bf07d23a6467
MarkFS15 <64194752+MarkFS15@users.noreply.github.com>
2024-02-20 23:34:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
-        "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "Wishing you a fabulous 21st birthday filled with joy.",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "Happy 21st! Shine bright, dream big, live fully.",  // 同上...
+        "Wishing you a fabulous 21st birthday filled with joy.",
+        "Happy Birthday! Radiate positivity, live passionately.",
+        "Celebrate 21! May joy, success, and love follow you.",
+        "21 and thriving! Wishing you endless smiles and adventures.",
+        "Happy 21st! Create memories, live boldly, love deeply.",
+        "Wishing you a fabulous 21st birthday filled with joy.",
+        "Happy 21! May your journey ahead be extraordinary.",
+        "Celebrate 21! Life's an adventure, enjoy the ride!",
+        "Have a Blessed Birthday!!",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -25,18 +25,18 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "心爱的小可爱": "",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
+        turn_on: "Let's Go",
         play: "音乐",
-        bannar_coming: "颜色",
+        bannar_coming: "Colours",
         balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
+        cake_fadein: "Cake？",
         light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        wish_message: "Happy Birthday Christine!",
         story: "A MESSAGE FOR YOU",
     }
 };
